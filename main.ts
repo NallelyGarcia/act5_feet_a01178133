2a416c9b5be1909dc8aebb52ed43d16b63390b12
NallelyGarcia <70342524+NallelyGarcia@users.noreply.github.com>
2020-09-10 13:52:00 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,20 +7,33 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Yards")
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    centimeters = feet * 30.48
+    basic.showNumber(centimeters)
+    basic.showString("Centimeters")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(feet)
     basic.showString("Feet")
     basic.showString("=")
-    inches = feet / 12
+    inches = feet * 12
     basic.showNumber(inches)
     basic.showString("Inches")
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    basic.showNumber(feet)
+    basic.showString("Feet")
+    basic.showString("=")
+    centimeters = feet * 30.48
+    meters = centimeters / 100
+    basic.showNumber(meters)
+    basic.showString("Meters")
 })
+let meters = 0
 let inches = 0
+let centimeters = 0
 let yards = 0
 let feet = 0
 feet = randint(1, 100)
